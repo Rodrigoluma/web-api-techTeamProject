c4511c348f1403cf8ffaf0f8df46b71534814759
Mário Cesar <mc.fialho@hotmail.com>
2022-07-27 19:49:24 -0300

    return to init

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 const port = normalizaPort(process.env.PORT || '3000');
 const mysql = require('mysql2');
 
+
+
 app.use(express.json());
 app.use(cors());
 
@@ -20,17 +22,38 @@ app.get('/doador', (req, res) => {
     execSQLQuery('SELECT * FROM doador', res);
 });
 
-//filtra os fornecedores pela cidade
-
-app.get('/doador/:id?', (req, res) => {
-    let { id_pontocoleta } = req.params.id;
-    execSQLQuery('SELECT * FROM doador WHERE id_pontocoleta = ?', [id_pontocoleta]);
+app.get('/doador/:areaAtuacao?', (req, res) => {    
+    const {areaAtuacao} = req.params;
+    console.log(areaAtuacao);
+    let filter = '';
+    if (areaAtuacao) filter = ` WHERE areaAtuacao = '${areaAtuacao}'`;
+    execSQLQuery('SELECT * FROM doador' + filter, res);
 });
 
 app.post('/doador', (req, res) => {
-    const { nome, telefone, area } = req.body;    
-    execSQLQuery(`INSERT INTO doador(nome, telefone, areaAtuacao) VALUES(?,?,?)`, [nome, Number(telefone), area]);
+    const {nome, areaAtuacao} = req.body;
+    execSQLQuery(`INSERT INTO doador(nome, areaAtuacao) VALUES('${nome}', '${areaAtuacao}')`, res);
 });
+
+// app.get('/doador', (req, res) => {
+//     connection.query('SELECT * FROM doador', function(err, results, fields) {
+//         console.log(results); // results contains rows returned by server
+//         console.log(fields); // fields contains extra meta data about results, if available
+//     });
+//     return res;
+// });
+
+// //filtra os fornecedores pela cidade
+
+// app.get('/doador/:id', async (req, res) => {
+//     let { id_pontocoleta } = req.params.id;
+//     await execSQLQuery('SELECT * FROM doador WHERE id_pontocoleta = ?', [id_pontocoleta]);
+// });
+
+// app.post('/doador', async (req, res) => {
+//     const { nome, telefone, area } = req.body;    
+//     await execSQLQuery(`INSERT INTO doador(nome, telefone, areaAtuacao) VALUES(?,?,?)`, [nome, Number(telefone), area]);
+// });
 
 // app.patch('/fornecedores/:id', (req, res) => {
 //     const id = parseInt(req.params.id);
@@ -63,20 +86,30 @@ app.listen(port, function () {
 
 
 
-function execSQLQuery(sqlQry, res){
+async function execSQLQuery(querySql, response){ 
+    if(querySql === undefined){
+        return
+    }  
     const connection = mysql.createConnection({
         host     : process.env.HOST,
         user     : process.env.USER,
         password : process.env.PASSWORD,
         database : process.env.DATABASE
     });
-   
-    connection.query(sqlQry, (error, results, fields) => {
+   console.log(querySql);
+    connection.query(querySql, (error, results, fields) => {
         if(error) 
-          res.json(error);
+          response.json(error);
         else
-          res.json(results);
+          response.json(results);
         connection.end();
         console.log('executou!');
     });
+    // if(global.connection && global.connection.state !== 'disconnected')
+    //     return global.connection;
+ 
+    
+    // console.log("Conectou no MySQL!");
+    // global.connection = connection;
+    // return connection;
 }
