3e5a1e5025359f7354a589bae39e99ef4f6b1241
Mário Cesar <mc.fialho@hotmail.com>
2022-07-29 00:46:58 -0300

    try fixed cors error

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ const port = normalizaPort(process.env.PORT || '3000');
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
 
